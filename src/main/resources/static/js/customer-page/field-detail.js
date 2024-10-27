6c792f818e5fe6c44de9df24c0608f012a533d8e
trieuden <119589795+trieuden@users.noreply.github.com>
2024-10-27 15:38:56 +0700

    update sport field list

--- src/main/resources/static/js/customer-page/field-detail.js
+++ src/main/resources/static/js/customer-page/field-detail.js
@@ -1,32 +1,46 @@
 let bookingDetail = document.getElementById("booking-detail");
 let buttonOrder = document.getElementById("buttonOrder");
 let buttonCloseBookingDetail = document.getElementById("buttonCloseBookingDetail");
-let container = document.getElementById('field-detail')
-
+let container = document.getElementById("field-detail");
+let id = container.getAttribute("fieldId");
 async function loadDetail() {
     try {
-        let container = document.getElementById('field-detail')
-        let id = container.getAttribute('fieldId');
+        const fieldRes = await fetch(`${SERVER_DOMAIN}/sports-field/${id}`);
+        const field = await fieldRes.json();
 
-        const response = await fetch(`${SERVER_DOMAIN}/sports-field/${id}`);
-        const data = await response.json();
-        console.log(data);
-        
-        await appendDetail(data)
-
+        await appendDetail(field);
+        await tab_detail(field)
     } catch (error) {
-        console.error('Error fetching detail:', error);
+        console.error("Error fetching detail:", error);
     }
 }
 
-async function appendDetail(data) {
-    document.getElementById('field_detail.fieldName').textContent = data.name
-    document.getElementById('field_detail.location').textContent = data.location
-    document.getElementById('field_detail.phone').textContent = data.phone
-    document.getElementById('field_detail.email').textContent = data.email
+async function appendDetail(field) {
+    //information
+    document.getElementById("field_detail.fieldName").textContent = field.name;
+    document.getElementById("field_detail.location").textContent = field.location;
+    document.getElementById("field_detail.phone").textContent = field.owner.mobileNumber;
+    document.getElementById("field_detail.email").textContent = field.owner.email;
+    document.getElementById("field_detail.category").textContent = field.category;
+    document.getElementById("field_detail.ownerName").textContent = field.owner.firstName + " " + field.owner.lastName;
+    document.getElementById("field_detail.ownerImage").src = field.owner.avatar;
+    document.getElementById("field_detail.rating").textContent = field.rating;
+    document.getElementById("field_detail.image").src = field.images[0];
 
+        // ảnh nhỏ phía dưới
+    let imageItems = document.getElementById("field_detail.imageItems");
+    for (let i = 0; i < 2; i++) {
+        let imageElement = document.createElement("img");
+        imageElement.src = field.images[i];
+        imageElement.className = "rounded-lg w-12 h-12 mr-3 opacity-50 cursor-pointer";
+        imageItems.appendChild(imageElement);
+    }
+    // button tab
+
+    document.getElementById("field_detail.button_tab_detail").href = "/sports-field-booking/sports-field/" + field.id + "/details";
+    document.getElementById("field_detail.button_tab_review").href = "/sports-field-booking/sports-field/" + field.id + "/reviews";
 }
-loadDetail()
+loadDetail();
 if (buttonOrder) {
     buttonOrder.addEventListener("click", () => {
         window.scrollTo({
@@ -46,3 +60,29 @@ if (buttonCloseBookingDetail) {
     });
 }
 
+async function tab_detail(field) {
+    let tab_detail = document.getElementById("field_detail.tabDetail");
+
+    if (tab_detail) {
+        let statusContainer = document.getElementById("field_detail.tabDetail.status");
+        let statusElements = statusContainer.querySelectorAll("div");
+        switch(field.status) {
+            case 'OPEN':
+                statusElements[0].classList.add('bg-yellow-100')
+                statusElements[0].querySelector('span').classList.add('text-green-800')
+                break;
+            case 'CLOSE':
+                statusElements[1].classList.add('bg-yellow-100')
+                statusElements[1].querySelector('span').classList.add('text-green-800')
+                break;
+            case 'MAINTENANCE':
+                statusElements[2].classList.add('bg-yellow-100')
+                statusElements[2].querySelector('span').classList.add('text-green-800')
+                break;
+            default:
+                statusElements[3].classList.add('bg-yellow-100')
+                statusElements[3].querySelector('span').classList.add('text-green-800')
+                break;
+        }
+    }
+}
--- src/main/resources/static/js/customer-page/sport-field.js
+++ src/main/resources/static/js/customer-page/sport-field.js
@@ -1,38 +1,112 @@
-let offset = 0;
+let currentOffset = 0;
 const limit = 12;
+let currentTab = "grid";
 
-async function loadSportFieldGrid() {
+let page = document.getElementById("sportsField.page");
+let sportFieldList = document.getElementById("sportsField.list");
+let sportFieldGrid = document.getElementById("sportsField.grid");
+
+async function loadSportFieldList(type, offset) {
     try {
         const response = await fetch(`${SERVER_DOMAIN}/sports-field?offset=${offset}&limit=${limit}`);
         const data = await response.json();
-        await appendField(data.items);
+        if(data.items.length){
+            if (type == "grid") await appendFieldGrid(data.items);
+            else await appendFieldList(data.items);
+            currentOffset = offset
+            loadPage(currentOffset);
+        }
+
+        
     } catch (error) {
         console.error("Error fetching sport field:", error);
     }
 }
 
-async function appendField(data) {
-    let sportFieldList = document.getElementById("sport-field-list");
+async function appendFieldGrid(data) {
+    sportFieldList.className = "hidden w-full text-left border-collapse";
+    sportFieldGrid.style.display = "grid";
 
-    data.forEach(field => {
-        const fieldElement = document.createElement('div');
+    sportFieldGrid.innerHTML = "";
+    data.forEach((field) => {
+        const fieldElement = document.createElement("div");
         fieldElement.innerHTML = `
-                <img src="https://placehold.co/400x200" alt="Sport field image" class="w-400 h-300 object-cover" />
-                <div class="p-4">
-                    <h4 class="font-bold text-xl">Manchester City Academy</h4>
-                    <p class="text-gray-600">24.000/đ</p>
-                    <p class="text-gray-500">Best fields in the world</p>
-                    <div class="mt-2 flex space-x-2">
-                        <span>⭐ 4.2/5</span>
-                    </div>
-                </div>
-                <a
+            <a
                 href="/sports-field-booking/sports-field/${field.id}/details"
                 class='bg-white shadow-lg rounded-lg overflow-hidden block cursor-pointer'
                 xmlns:th="http://www.w3.org/1999/xhtml"
-                >Detail</a>
-            `;            
-        sportFieldList.appendChild(fieldElement);
+            >
+                <img src="${field.images[0]}" alt="Sport field image" class="w-full h-[260px]" />
+                <div class="p-4">
+                    <h4 class="font-bold text-xl">${field.name}</h4>
+                    <p class="text-red-600">24.000/đ</p>
+                    <textarea class="text-gray-500 w-full resize-none overflow-hidden" rows='1' readonly >${field.location}</textarea>
+                    <div class="mt-2 flex space-x-2">
+                        <span>⭐ ${field.rating}/5</span>
+                    </div>
+                </div>
+            </a>
+            `;
+        sportFieldGrid.appendChild(fieldElement);
     });
 }
-loadSportFieldGrid();
+async function appendFieldList(data) {
+    try {
+        sportFieldGrid.style.display = "none";
+        sportFieldList.className = "w-full text-left border-collapse";
+
+        let sportFieldTable = sportFieldList.querySelector("tbody");
+        sportFieldTable.innerHTML = ''
+        data.forEach((field) => {
+            const fieldElement = document.createElement("tr");
+            fieldElement.className = "border-b";
+            fieldElement.innerHTML = `
+                    <td class="p-4">${field.name}</td>
+                    <td class="p-4">
+                        <img src="${field.images[0]}" alt="Stadium Image" class='h-10 w-10' />
+                    </td>
+                    <td class="p-4">${field.location}</td>
+                    <td class="p-4">${field.category}</td>
+                    <td class="p-4">5000</td>
+                    <td class="p-4">${field.owner.firstName + ' ' + field.owner.lastName}</td>
+                    <td class="p-4">08:00 AM</td>
+                    <td class="p-4">10:00 PM</td>
+                    <td class="p-4">
+                        <span class="bg-green-100 text-green-600 py-1 px-3 rounded-full text-xs"
+                            th:text="#{dashboard.sportfield_status.active}">${field.status}</span>
+                    </td>
+                    <td class="p-4">
+                        <a  href="/sports-field-booking/sports-field/${field.id}/details">
+                            <i class="fa-solid fa-share-from-square text-green-500"></i>
+                        </a>
+                    </td>
+            `;
+            sportFieldTable.appendChild(fieldElement);
+        });
+    } catch (error) {
+        console.error("Error fetching sport field:", error);
+    }
+}
+loadSportFieldList(currentTab, 0);
+
+document.getElementById("sportsField.button-tab-grid").addEventListener("click", () => {
+    currentTab = "grid";
+    loadSportFieldList(currentTab, currentOffset);
+});
+document.getElementById("sportsField.button-tab-list").addEventListener("click", () => {
+    currentTab = "list";
+    loadSportFieldList(currentTab, currentOffset);
+});
+
+//change page
+
+async function loadPage(offset) {
+    page.textContent = offset + 1;
+}
+
+document.getElementById("sportsField.nextPage").addEventListener("click", () => {
+    loadSportFieldList(currentTab, currentOffset + 1);
+});
+document.getElementById("sportsField.backPage").addEventListener("click", () => {    
+    loadSportFieldList(currentTab, currentOffset -1);
+});
