131fa8c4f301f500a6175c6d8f68774fcc109237
trieuden <119589795+trieuden@users.noreply.github.com>
2024-11-25 22:12:27 +0700

    form add availability access

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -281,3 +281,14 @@ function addTooltip(element, tooltipText) {
     }
     
 }
+
+function formatDateInputValue(dateString) {
+    const date = new Date(dateString);
+
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+
+    // Định dạng theo chuẩn YYYY-MM-DD
+    return `${year}-${month}-${day}`;
+}
